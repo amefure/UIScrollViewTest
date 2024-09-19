210f884c6877fa4e31740347bfc003da90f13754
amefure <shachi10969@gmail.com>
2024-09-19 20:56:50 +0900

    Initial Commit

---
+++ TestUIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestUIKitApp
+//
+//  Created by t&a on 2024/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
